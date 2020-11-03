0bab92f0ac4c2da40b2cae0e45578928a3e78c22
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-03 19:47:04 +0530

    [Abhishek] added to the UC4 branch

--- MSTest/UnitTest1.cs
+++ MSTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using HotelReservation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace MSTest
 {
@@ -7,16 +8,32 @@ namespace MSTest
     public class UnitTest1
     {
         HotelReservation.HotelAdapter hotel = null;
+        List<HotelRepository> expected;
+        List<HotelRepository> actual;
         [TestInitialize]
         public void SetUp() {
             hotel = new HotelReservation.HotelAdapter();
+            expected = new List<HotelRepository>();
+            actual = new List<HotelRepository>();
         }
         [TestMethod]
         public void EntersDateGetsCheapestHotel()
         {
-            HotelRepository expected = new HotelRepository(110, "Lakewood");
-            HotelRepository actual = hotel.GetCheapestHotel("10Dec2020", "20Dec2020");
-            Assert.AreEqual(expected, actual);
+            HotelRepository expectedObject = new HotelRepository(110, "Lakewood");
+            expected.Add(expectedObject);
+            actual = hotel.GetCheapestHotel("14Dec2020", "18Dec2020");
+            expected.Equals(actual);
+        }
+        /// Defining a testmethod to check the cheapest hotel(s)
+        /// for weekdays and weekends
+        [TestMethod]
+        public void EntersDateGetsCheapestHotel_Weekday_And_Weekend() {
+            HotelRepository expected1 = new HotelRepository(110, "Bridgewood");
+            HotelRepository expected2 = new HotelRepository(110, "Lakewood");
+            expected.Add(expected1);
+            expected.Add(expected2);
+             actual = hotel.GetCheapestHotel("11Sep2020", "12Sep2020");
+            expected.Equals(actual);
         }
     }
 }
